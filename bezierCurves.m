88927f817d7d9d144498f068c5d8422af1e0e8f1
Simen Bootsma <s.t.bootsma@student.utwente.nl>
2018-10-10 12:53:12 +0200

    Simplify file to max 1 obstacle

--- bezierCurves.m
+++ bezierCurves.m
@@ -1,108 +1,78 @@
-clear all; clc; close all;
-% This works for all orientations
-% This only works for an initial path that goes between the centers of two 
-% overlapping obstacles
-% Start and end points cannot be too close to the obstacles
-
-% TODO: take in account physical limitations
-% TODO: set initial velocity to zero
-% TODO: multiple opponents in different configurations
-% TODO: take into consideration orientation of opponent? (nice addition
-% maybe)
-% TODO: make calculation for more than 2 obstacles
+clear; clc; %clf(1); clf(2);
+% The purpose of this file is to make the planned path responsive to moving
+% obstacles. Only for 1 obstacle atm.
 
 %% Set variables
 dt = 1/2000; % normalized time step
 minRadius=25; % circle around object
-startOrientationAngle=0.5*pi; % 0-2pi
-orientationMargin=20; % distance for the orientation control point
 ptStart=[5 5]; % start position
 ptEnd=[100 100]; % ball position
-ptGoal=[110 130]; % point where the ball should go to
-
-% Random number stuff
-a=ptStart(1)-10; b=ptEnd(1);
-ptObject=[rand(1,2);rand(1,2)]*(b-a)+5;
-
-% ptObject=[60 30; 20 50]; % objects that are in the way
+ptGoal=[130 80]; % point where the ball should go to
+ptObject=[60 65]; % objects that are in the way
 [nObstacles,~]=size(ptObject);
+
+% State variables
+initVel = 0;
+initAng = 0.5*pi;
+finalVel = 0;
+finalAng = atan2(ptGoal(2)-ptEnd(2), ptGoal(1)-ptEnd(1));
+orientDist = 30; % could be used to limit the angular velocity, for now to make the graph more pretty
+
  
-%% Orientation points
-h=orientationMargin*sin(startOrientationAngle);
-l=orientationMargin*cos(startOrientationAngle);
-ptStartOrientation=[ptStart(1)+l, ptStart(2)+h];
+%% Set initial and final states
+if round(initVel) == 0 % Should be limited and not rounded
+    ptInitState = ptStart + orientDist*[0, 0; cos(initAng), sin(initAng)];
+else
+    NumPoints = 4; % Start, initial state, final state, end
+    ptInitState = ptStart + initVel/(NumPoints-1)*[cos(initAng), sin(initAng)];
+end
 
-endOrientationDistance=sqrt((ptGoal(1)-ptEnd(1))^2+(ptGoal(2)-ptEnd(2))^2)+orientationMargin;
-endOrientationAngle=atan(abs((ptGoal(2)-ptEnd(2)))/abs((ptGoal(1)-ptEnd(2))));
-
-ptEndOrientation=ptEnd+(ptGoal-ptEnd)*[cos(pi), -sin(pi); sin(pi), cos(pi)];
+if round(finalVel) == 0 % Should be limited and not rounded
+    ptFinalState = ptEnd + orientDist*[cos(pi-finalAng), -sin(pi-finalAng); 0, 0];
+else
+    NumPoints = 4; % Start, initial state, final state, end
+    ptFinalState = ptEnd + finalVel/(NumPoints-1)*[cos(pi-finalAng), -sin(pi-finalAng)];
+end
 
 %% Initial path
-pts=[ptStart; ptStartOrientation; ptEndOrientation; ptEnd];
+pts=[ptStart; ptInitState; ptFinalState; ptEnd];
 [initX, initY]=bezierCurve(pts, dt);
 ptDanger=findDanger(ptObject, initX, initY, minRadius); % check if circle crosses path
 [nDanger,~]=size(ptDanger); % amount of danger points
 
-%% Points of first half of path
-ptInter=createInter(nDanger, ptDanger, ptStart, ptEnd, minRadius, ptStartOrientation);
+%% Avoid obstacle by adding control point
+[ptControl]=createCP(ptDanger, initX, initY, minRadius);
+if round(initVel) > 0 && ~isempty(ptControl) % Should be limited and not rounded
+    NumPoints = 4 + length(ptControl(:,1)); % Same as before + number of control points
+    ptInitState = ptStart + initVel/(NumPoints-1)*[cos(initAng), sin(initAng)];
+end
+pts=[ptStart; ptInitState; ptControl; ptFinalState; ptEnd];
 
-pts=[ptStart; ptStartOrientation; ptInter];
-[firstX, firstY]=bezierCurve(pts, dt);
-[ptInterObject]=findDanger(ptDanger, firstX, firstY, minRadius);
-[ptControl]=createCP(ptInterObject, firstX, firstY, minRadius);
-ptsFirst=[ptStart; ptStartOrientation; ptControl; ptInter];
-
-% Points of second half of path
-ptsSecond=[ptInter; ptEndOrientation; ptEnd];
-
-% Bezier curves
-% Calculate points on both sides of Inter Point to assure continuity
-n1=length(ptsFirst(:,1)); % degree of first curve
-n2=length(ptsSecond(:,1)); % degree of second curve
-
-a=n1/n2;
-b=(n1-1)/(n2-1);
-
-ptInterOrientation1=(1+0.5/a+0.5*b)/(1+b)*ptInter+b/(2+2*b)*ptsFirst(end-1,:)-1/(2*a+2*a*b)*ptsSecond(2,:);
-ptInterOrientation2=(1+a)*ptInter-a*ptInterOrientation1;
-
-ptsFirst=[ptsFirst(1:end-1,:); ptInterOrientation1; ptsFirst(end,:)];
-ptsSecond=[ptsSecond(1,:); ptInterOrientation2; ptsSecond(2:end,:)];
-
-%% Calculate Bezier Curves
-[firstX, firstY]=bezierCurve(ptsFirst, dt);
-[secondX, secondY]=bezierCurve(ptsSecond, dt);
+%% Calculate Bezier Curve
+[X, Y]=bezierCurve(pts, dt);
 
 %% Other stuff
 % Calculate data for analysis
-firstV=getVelocity(ptsFirst, dt);
-firstAng=getRotation(firstV);
-secondV=getVelocity(ptsSecond, dt);
-secondAng=getRotation(secondV);
-
-% Combine halfs
-X=[firstX, secondX];
-Y=[firstY, secondY];
-V=[firstV, secondV];
-ang=[firstAng, secondAng];
+V=getVelocity(pts, dt);
+A=getAcceleration(pts, dt);
+ang=getRotation(V);
 angVel=diff(ang)/dt;
+
 
 
 %% Visuals
 figure(1)
-set(gcf,'Position',[1367 -255 1280 1026]) % to put figure on second monitor, selina laptop
+%set(gcf,'Position',[1367 -255 1280 1026]) % to put figure on second monitor, selina laptop
 subplot(2,3,[1,2,4,5])
 hold on
-plot(X, Y, '--', 'color', [0.6, 0.6, 0.6])
+plot(initX, initY, '--', 'color', [0.6, 0.6, 0.6])
+plot(X, Y, '--k')
 plot(ptStart(1),ptStart(2),'g*');
 plot(ptEnd(1),ptEnd(2),'r*');
-if ptInter(1)~=0 && ptInter(2)~=0
-    plot(ptInter(1),ptInter(2),'*','color',[1 0.5 0])
-end
 if ~isempty(ptControl)
     plot(ptControl(:,1),ptControl(:,2),'b*')
 end
-plot([ptStartOrientation(1),ptEndOrientation(1)],[ptStartOrientation(2),ptEndOrientation(2)],'m*');
+plot([ptInitState(2,1), ptFinalState(1,1)],[ptInitState(2,2), ptFinalState(1,2)],'m*');
 plot(ptGoal(1),ptGoal(2),'c*');
 if ~isempty(ptObject)
     plot(ptObject(:,1),ptObject(:,2),'k*')
@@ -114,9 +84,9 @@ ylim([-50 ptGoal(2)+50]); xlim([-50 ptGoal(1)+20]);
 grid on
 axis equal
 if ~isempty(ptControl)
-    legend('Path', 'Start point', 'End Point', 'Intermediate point', 'Control Points', 'Orientation points', 'Goal', 'Objects', 'location', 'best')
+    legend('Initial path', 'Path', 'Start point', 'End Point', 'Control points', 'Orientation points', 'Goal', 'Objects', 'location', 'best')
 else
-    legend('Path', 'Start point', 'End Point', 'Intermediate point', 'Orientation points', 'Goal', 'Objects', 'location', 'best')
+    legend('Initial path', 'Path', 'Start point', 'End Point', 'Orientation points', 'Goal', 'Objects', 'location', 'best')
 end
 hold off
 
@@ -126,19 +96,28 @@ plot(T,V(1,:),T,V(2,:),T,sqrt(V(1,:).^2 + V(2,:).^2))
 grid on
 xlabel('Normalized time')
 ylabel('Velocity')
-legend('V_x','V_y','V_t_o_t')
-
-% subplot(2,3,5)
-% plot(T,ang);
-% grid on
-% xlabel('Normalized time')
-% ylabel('Angle [rad]')
+legend('V_x','V_y','V_t_o_t','location','best')
 
 subplot(2,3,6)
+plot(T,A(1,:),T,A(2,:),T,sqrt(A(1,:).^2 + A(2,:).^2))
+grid on
+xlabel('Normalized time')
+ylabel('Acceleration')
+legend('A_x','A_y','A_t_o_t','location','best')
+
+figure(2)
+subplot(1,2,1)
+plot(T,ang);
+grid on
+xlabel('Normalized time')
+ylabel('Angle [rad]')
+
+subplot(1,2,2)
 plot(T(2:end),angVel);
 grid on
 xlabel('Normalized time')
 ylabel('Angular velocity [rad/(a.u.)]')
+
 
 
 %% Functions
@@ -162,52 +141,6 @@ for i = 0:n
 end
 end
 
-function [ptInter]=createInter(nDanger, obj, ptStart, ptEnd, minRadius, ptStartOrientation)
-% Decide whether to go left or right past objects
-startToObj=ones(nDanger,2); angToObj=ones(nDanger,2); dist=ones(nDanger,2);
-
-startToOr=ptStartOrientation-[ptStart(1), ptEnd(1)];
-startToOr=startToOr/norm(startToOr);
-for i=1:nDanger
-    startToObj(i,:)=obj(i,:)-[ptStart(1), ptEnd(1)];
-    startToObj(i,:)=startToObj(i,:)/norm(startToObj(i,:));
-    angToObj(i)=acos(dot(startToOr, startToObj(i,:)));
-    dist(i)=sqrt((norm([ptStart(end), ptEnd(end)]-obj(i,:)))^2-minRadius^2);
-end
-if nDanger==0
-    return;
-elseif nDanger==1
-    ang=90-(180-90-atan(obj(2)/obj(1)));
-    ptInter=[obj(1)+sin(ang)*minRadius obj(2)+cos(ang)*minRadius]; % should be better
-elseif nDanger==2
-    if  angToObj(1) < angToObj(2)
-        % It's faster to go past object 1
-        % Construct intermediary point
-        if atan((obj(1,2)-ptEnd(end))/(obj(1,1)-ptStart(end))) > atan((obj(2,2)-ptEnd(end))/(obj(2,1)-ptStart(end)))
-            or=-1;
-        else
-            or=1;
-        end
-        selfToObject=obj(1,:)-[ptStart(end), ptEnd(end)];
-        ang=or*atan(minRadius/dist(1));
-        ptInter=[ptStart(end), ptEnd(end)]+dist(1)*selfToObject/norm(selfToObject)*[cos(ang), -sin(ang); sin(ang), cos(ang)];
-    else
-        % It's faster to go past object 2
-        % Construct intermediary point
-        if atan((obj(1,2)-ptEnd(end))/(obj(1,1)-ptStart(end)))>atan((obj(2,2)-ptEnd(end))/(obj(2,1)-ptStart(end)))
-            or=1;
-        else
-            or=-1;
-        end
-        selfToObject=obj(2,:)-[ptStart(end), ptEnd(end)];
-        ang=or*atan(minRadius/dist(2));
-        ptInter=[ptStart(end), ptEnd(end)]+dist(2)*selfToObject/norm(selfToObject)*[cos(ang), -sin(ang); sin(ang), cos(ang)];
-    end
-elseif nDanger>2
-    disp('More than 2 objects????? I dont know man');
-end
-end
-
 function [CP] = createCP(obj, BZ_X, BZ_Y, minRadius)
 % Assumes all objects are dangerous
 CP=[];
@@ -225,37 +158,6 @@ elseif length(obj(:,1))==1
         vecToCurve=[cur_x, cur_y]-[x, y];
         CP=[cur_x, cur_y]+3*(minRadius-norm(vecToCurve))*vecToCurve/norm(vecToCurve);
         return;
-    end
-elseif length(obj(:,1))==2
-    if norm(obj(1,:)-obj(2,:))<2*minRadius
-        % If their circles are overlapping
-        % Decide whether to go left or right past the 2 objects
-        dist1=sqrt((norm([BZ_X(end), BZ_Y(end)]-obj(1,:)))^2-minRadius^2);
-        dist2=sqrt((norm([BZ_X(end), BZ_Y(end)]-obj(2,:)))^2-minRadius^2);
-        if  dist1 < dist2
-            % It's faster to go past object 1.
-            % Construct intermediary point
-            if atan((obj(1,2)-BZ_Y(end))/(obj(1,1)-BZ_X(end))) > atan((obj(2,2)-BZ_Y(end))/(obj(2,1)-BZ_X(end)))
-                or=-1;
-            else
-                or=1;
-            end
-            selfToObject=obj(1,:)-[BZ_X(end), BZ_Y(end)];
-            ang=or*atan(minRadius/dist1);
-            ptInter=[BZ_X(end), BZ_Y(end)]+dist1*selfToObject/norm(selfToObject)*[cos(ang), -sin(ang); sin(ang), cos(ang)];
-        else
-            % It's faster to go past object 2.
-            % Construct intermediary point
-            if atan((obj(1,2)-BZ_Y(end))/(obj(1,1)-BZ_X(end)))>atan((obj(2,2)-BZ_Y(end))/(obj(2,1)-BZ_X(end)))
-                or=1;
-            else
-                or=-1;
-            end
-            selfToObject=obj(2,:)-[BZ_X(end), BZ_Y(end)];
-            ang=or*atan(minRadius/dist2);
-            ptInter=[BZ_X(end), BZ_Y(end)]+dist2*selfToObject/norm(selfToObject)*[cos(ang), -sin(ang); sin(ang), cos(ang)];
-        end
-        CP=ptInter;
     end
 else
     disp("HELP! So much danger, I don't know what to do...YET");
@@ -279,7 +181,9 @@ for i=1:length(obj(:,1))
         for j=1:length(obj(:,1))
             if i~=j && norm(obj(i,:)-obj(j,:))<2*minRadius
                 for k=1:length(BZ_X)
-                    if dot(obj(i,:)-[BZ_X(k), BZ_Y(k)], obj(j,:)-[BZ_X(k), BZ_Y(k)])<-0.95
+                    pathToObj1 = (obj(i,:)-[BZ_X(k), BZ_Y(k)])/norm(obj(i,:)-[BZ_X(k), BZ_Y(k)]);
+                    pathToObj2 = (obj(j,:)-[BZ_X(k), BZ_Y(k)])/norm(obj(j,:)-[BZ_X(k), BZ_Y(k)]);
+                    if dot(pathToObj1, pathToObj2)<-0.95
                         dangerous=[dangerous; obj(j,:)];
                     end
                 end
@@ -307,7 +211,39 @@ for i=0:n
     dYdT=dYdT+nchoosek(n,i)*(i*T.^(i-1).*(1-T).^(n-i)+T.^i.*-(n-i).*(1-T).^(n-i-1))*P(i+1,2);
 end
 
+% Fix NaN data by extrapolation
+dXdT(1) = dXdT(2) - (dXdT(3)-dXdT(2));
+dXdT(end) = dXdT(end-1) + (dXdT(end-1)-dXdT(end-2));
+dYdT(1) = dYdT(2) - (dYdT(3)-dYdT(2));
+dYdT(end) = dYdT(end-1) + (dYdT(end-1)-dYdT(end-2));
+
 V=[dXdT; dYdT];
+end
+
+function [A]=getAcceleration(P, dt)
+T = 0:dt:1;
+d2XdT2 = zeros(1,length(T));
+d2YdT2 = zeros(1,length(T));
+
+if isempty(P)
+    disp('Please insert control points!');
+    return;
+end
+
+n = length(P(:,1)) - 1; % degree of polynomial
+
+for i = 0:n
+    d2XdT2 = d2XdT2 + nchoosek(n,i) * (i*(i-1)*T.^(i-2).*(1-T).^(n-i) - 2*i*(n-i)*T.^(i-1).*(1-T).^(n-i-1) + (n-i)*(n-i-1)*T.^i.*(1-T).^(n-i-2)) * P(i+1,1);
+    d2YdT2 = d2YdT2 + nchoosek(n,i) * (i*(i-1)*T.^(i-2).*(1-T).^(n-i) - 2*i*(n-i)*T.^(i-1).*(1-T).^(n-i-1) + (n-i)*(n-i-1)*T.^i.*(1-T).^(n-i-2)) * P(i+1,2);
+end
+
+% Fix NaN data by extrapolation
+d2XdT2(1) = d2XdT2(2) - (d2XdT2(3)-d2XdT2(2));
+d2XdT2(end) = d2XdT2(end-1) + (d2XdT2(end-1)-d2XdT2(end-2));
+d2YdT2(1) = d2YdT2(2) - (d2YdT2(3)-d2YdT2(2));
+d2YdT2(end) = d2YdT2(end-1) + (d2YdT2(end-1)-d2YdT2(end-2));
+
+A = [d2XdT2; d2YdT2];
 end
 
 function [ang]=getRotation(V)
@@ -320,4 +256,8 @@ for i=1:length(V(1,:))
         ang(i)=ang(i)+pi;
     end
 end
+
+% Fix NaN data by extrapolation
+ang(1) = ang(2) - (ang(3)-ang(2));
+ang(end) = ang(end-1) + (ang(end-2)-ang(end-3));
 end
