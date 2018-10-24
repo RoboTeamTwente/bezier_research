dd1416e237106536756b5b84c2166ecd25ae9ce4
Selina <s.zwerver@student.utwente.nl>
2018-10-24 15:53:25 +0200

    Now makes circles with right radius

--- plotter.m
+++ plotter.m
@@ -1,4 +1,4 @@
-function plotter(allComb, center, path, ptObject, curve, v0)
+function plotter(allComb, center, path, ptObject, curve, v0, nObjects)
 % Plot all points and line segments for debugging purposes
 hold on
 %% Lines
@@ -16,7 +16,8 @@ plot(curve(1,:),curve(2,:),'-m','linewidth',2);
 quiver(center(end-1,2),center(end-1,3),v0.amp*cos(v0.theta),v0.amp*sin(v0.theta),'color','b','linewidth',3);
 
 %% Points
-plot(ptObject(:,1),ptObject(:,2),'xr','MarkerSize',20); plot(ptObject(:,1),ptObject(:,2),'or','MarkerSize',20);
+plot(ptObject(:,1),ptObject(:,2),'xr');
+viscircles([ptObject(:,1) ptObject(:,2)], 18*ones(nObjects,1));
 plot(center(:,2),center(:,3),'.b','MarkerSize',15);
 plot(center(end-1,2),center(end-1,3),'.g','MarkerSize',25);
 plot(center(end,2),center(end,3),'.r','MarkerSize',25);
