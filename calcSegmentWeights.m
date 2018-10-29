6cb3c820aca46cc354c9e96724a1733fded56c28
Simen Bootsma <s.t.bootsma@student.utwente.nl>
2018-10-29 17:00:31 +0100

    Add end orientation to weight system

--- calcSegmentWeights.m
+++ calcSegmentWeights.m
@@ -8,12 +8,15 @@ endPolygon = allComb(allComb(:,2)==6494,3); % ID's of all points connected with 
 
 segment_weights = ones(length(allComb(:,1)),1);
 %% End weights
-% for i = 1:length(endPolygon)
-%     ID = endPolygon(i);
-%     endToNode = center(ID,2:3) - center(end,2:3);
-%     angleToNode = vf.theta-atan2(endToNode(2),endToNode(1));
-%     segment_weights(endIndex+i-1) = factor*cos(angleToNode/2)^2; % if angle is 0, result is 1. if angle is pi, result is 0
-% end
+for i = 1:length(endPolygon)
+    ID = endPolygon(i);
+    ind = find(center(:,1)==ID,1);
+    if ~isempty(ind)
+        endToNode = center(ind,2:3) - center(end,2:3);
+        angleToNode = vf.theta-atan2(endToNode(2),endToNode(1));
+        segment_weights(endIndex+i-1) = factor*cos(angleToNode/2)^2; % if angle is 0, result is 1. if angle is pi, result is 0
+    end
+end
 
 %% Start weights
 for i = 1:length(startPolygon)
