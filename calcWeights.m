069eaa6245f39b211c0015fe568beea3cab2146d
Simen Bootsma <s.t.bootsma@student.utwente.nl>
2018-10-29 14:25:06 +0100

    Fix indexing bug and remove end orientation

--- calcWeights.m
+++ calcWeights.m
@@ -4,22 +4,25 @@ endPolygon = allComb(allComb(:,2)==6494,3); % ID's of all points connected with 
 
 center_weights = ones(length(center(:,1)),1);
 %% End weights
-for i = 1:length(endPolygon)
-    ID = endPolygon(i);
-    endToNode = center(ID,2:3) - center(end,2:3);
-    angleToNode = vf.theta-atan2(endToNode(2),endToNode(1));
-    center_weights(ID) = cos(angleToNode/2)^2; % if angle is 0, result is 1. if angle is pi, result is 0
-end
+% for i = 1:length(endPolygon)
+%     ID = endPolygon(i);
+%     endToNode = center(ID,2:3) - center(end,2:3);
+%     angleToNode = vf.theta-atan2(endToNode(2),endToNode(1));
+%     center_weights(ID) = cos(angleToNode/2)^2; % if angle is 0, result is 1. if angle is pi, result is 0
+% end
 
 %% Start weights
 for i = 1:length(startPolygon)
     ID = startPolygon(i);
-    startToNode = center(ID,2:3) - center(end-1,2:3);
-    angleToNode = v0.theta-atan2(startToNode(2),startToNode(1));
-    center_weights(ID) = sin(angleToNode/2)^2; % if angle is 0, result is 0. if angle is pi, result is 1
+    ind = find(center(:,1)==ID,1);
+    if ~isempty(ind)
+        startToNode = center(ind,2:3) - center(end-1,2:3);
+        angleToNode = v0.theta-atan2(startToNode(2),startToNode(1));
+        center_weights(ind) = sin(angleToNode/2)^2; % if angle is 0, result is 0. if angle is pi, result is 1
+    end
 end
 
 % Set the weight of the end node to a low value, so that it goes to the end
 % point when the angle is quite okay
-center_weights(end) = 0.5;
+%center_weights(end) = 0.4;
 end
